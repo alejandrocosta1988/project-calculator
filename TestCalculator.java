4bd2ca43b0f7613bac89c71642568aec217a2d75
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-30 22:03:15 -0300

    tests if object Calculator is created

--- TestCalculator.java
+++ TestCalculator.java
@@ -1,1 +1,17 @@
+/** Tests for class Calculator
+* @author Alejandro Costa
+* @version 0.1
+**/
+public class TestCalculator
+{
+    public static void main(String[] args)
+    {
+        testCreation();
+    }
 
+    /** Tests if object of class Calculator can be created **/
+    public static void testCreation()
+    {
+        Calculator myCalculator = new Calculator();
+    }
+}
