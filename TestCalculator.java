797cb88cb11ae9ab1a1560446761694247f0ba35
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-30 22:30:01 -0300

    refactor: creation of Calculator as a static variable

--- TestCalculator.java
+++ TestCalculator.java
@@ -6,9 +6,15 @@ public class TestCalculator
 {
     public static void main(String[] args)
     {
+        System.out.println("Testing creation of object...");
         Calculator aCalculator = testCreation();
-        System.out.println("Result should be 0.0 >>> Result: " + aCalculator.getResult());
+        System.out.println("Object created");
+        System.out.println("Testing the initial result of the calculator...");
+        System.out.println("Result should be 0.0 >>> Result: " + testInitialResult());
+        System.out.println("OK");
     }
+
+    private static Calculator myCalculator = new Calculator();
 
     /** Tests if object of class Calculator can be created **/
     public static Calculator testCreation()
@@ -16,4 +22,14 @@ public class TestCalculator
         Calculator myCalculator = new Calculator();
         return myCalculator;
     }
+
+    /** Tests if the initial result of the calculator is zero **/
+    public static double testInitialResult()
+    {
+        double initialResult = myCalculator.getResult();
+        return initialResult;
+    }
+
+
+
 }
