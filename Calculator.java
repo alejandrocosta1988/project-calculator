489013c42540d5b8e14b25d04c9d016c8d0c1815
Alejandro Costa <alejandroesfc@gmail.com>
2021-11-30 22:13:49 -0300

    a Calculator is created with result set to zero

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+/** Implements a calculator with methods to add, subtract, multiply and divide
+* @author Alejandro Costa
+* @version 0.1 2021-11-30
+**/
+public class Calculator
+{
+
+    private double result;
+
+    public Calculator()
+    {
+        result = 0;
+    }
+
+    public double getResult()
+    {
+        return this.result;
+    }
+}
