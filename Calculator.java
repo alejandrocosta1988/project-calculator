b3ad7ef226c8f9ff619841bf96649d7ba713ac42
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-01 08:57:04 -0300

    documentation comments

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,9 @@
 **/
 public class Calculator
 {
-
+    /**
+    * {@value result} keeps track of the result of all computations. If you need to recompute everything, you should use the method eraseResults().
+    * **/
     private double result;
 
     public Calculator()
@@ -12,17 +14,28 @@ public class Calculator
         result = 0;
     }
 
+    /**
+    * Displays the result stored in the Calculator.
+    **/
     public double getResult()
     {
         return this.result;
     }
 
+    /**
+    * @param x A double or int
+    * @param y A double or int
+    * Adds a and b and stores the result of the computation in the Calculator result. Use method getResult() to access the result of the computation.
+    **/
     public void add(double x, double y)
     {
         double additionResult = x + y;
         this.result += additionResult;
     }
 
+    /**
+    * Erases the results of all computations. The value of result is set back to zero.
+    **/
     public void eraseResult()
     {
         this.result = 0;
