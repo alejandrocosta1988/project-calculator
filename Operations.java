1190b8ca0e00fdad954722af2f38268e3b3651bc
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-02 16:55:51 -0300

    methods added: subtarct, multiply and divide

--- Operations.java
+++ Operations.java
@@ -1,12 +1,32 @@
 /**
 * Class with methods to compute basic operations
+* @author Alejandro Costa
+* @version 0.1 2021-12-02
 **/
 public class Operations
 {
-    /* Returns a + b */
-    public double add(double a, double b)
+    /* Returns a plus b */
+    public static double add(double a, double b)
     {
         return a + b;
     }
 
+    /* Returns a minus b */
+    public static double subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    /* Returns a times b */
+    public static double multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    /* Returns a divided by b */
+    public static double divide(double a, double b)
+    {
+        return a / b;
+    }
+    
 }
