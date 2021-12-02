120785a1b024ee70971ec8ff5156a681b0a1bba8
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-02 16:24:43 -0300

    definition of class operation with methods for basic arithmetical computation

---
+++ Operations.java
@@ -0,0 +1,12 @@
+/**
+* Class with methods to compute basic operations
+**/
+public class Operations
+{
+    /* Returns a + b */
+    public double add(double a, double b)
+    {
+        return a + b;
+    }
+
+}
