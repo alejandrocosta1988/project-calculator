1d9461aa7186f52a526da7a2d4ca1d77e86b5390
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-03 20:04:45 -0300

    breaks the loop when user enter 'q'

---
+++ mainApp.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/*
+* Main Calculator application
+* @author Alejandro Costa
+* @version 0.1 2021-12-02
+*/
+public class mainApp
+{
+    public static void main(String[] args)
+    {
+
+        boolean continueComputation = true;
+
+        System.out.println(welcomeMessage);
+
+        while (continueComputation)
+        {
+          System.out.print(presentOptionToQuit);
+          String userOption = askIfUserWantsToQuit(in);
+          String quit = "q";
+          if (userOption.equals(quit))
+          {
+              break;
+          }
+        }
+
+    }
+
+    // messages to user
+    public static String welcomeMessage = "Welcome to Calculator!";
+    public static String presentOptionToQuit = "Enter 'q' if you want to quit.\n> ";
+
+    // inputs do usuário
+    static Scanner in = new Scanner(System.in);
+
+    public static String askIfUserWantsToQuit(Scanner input)
+    {
+        return input.next();
+    }
+
+}
