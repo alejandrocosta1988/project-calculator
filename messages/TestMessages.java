75d06b6e3027ee84d4f93e4a262a5d4a19480a5e
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-09 14:09:38 -0300

    tests the creation of an object of class Messages

---
+++ messages/TestMessages.java
@@ -0,0 +1,14 @@
+/**
+* Unit tests for class messages
+* @author Alejandro Costa
+* @version 0.1 2021-12-09
+*/
+public class TestMessages
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Creating object of class Messages...");
+        Messages messagesToUser = new Messages();
+        System.out.println("Done.");
+    }
+}
