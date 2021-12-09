28a2b3e43d38bc430ec7345e7fff10ca43582f9a
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-09 08:32:16 -0300

    creation of a class to hold all messages that may be sent to user

---
+++ messages/Messages.java
@@ -0,0 +1,13 @@
+/*
+* Class defined to keep all messages that may be sent to users of Calculator Application.
+* @author Alejandro Costa
+* @version 0.1 2021-12-07
+*/
+class Messages
+{
+    private String welcomeMessage = "\nWelcome to Calculator!";
+
+    public Messages{};
+
+    
+}
