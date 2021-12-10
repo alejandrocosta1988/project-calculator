f8b6666258d4605daba1bd9b56fe81b0bbcaa07d
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-10 14:21:32 -0300

    welcome message complete

--- messages/Messages.java
+++ messages/Messages.java
@@ -1,9 +1,10 @@
+package messages;
 /*
 * Class defined to keep all messages that may be sent to users of Calculator Application.
 * @author Alejandro Costa
 * @version 0.1 2021-12-07
 */
-class Messages
+public class Messages
 {
     private String welcomeMessage = "\nWelcome to Calculator!";
 
@@ -12,8 +13,7 @@ class Messages
     // Displays a welcome message to the user
     public void welcomeMessage()
     {
-        
-        System.out.print(this.welcomeMessage);
+        System.out.println(this.welcomeMessage);
     }
 
 }
