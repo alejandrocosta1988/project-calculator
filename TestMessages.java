a4816b895e3b3407bc8c4ba3e99f7605a2d1e923
Alejandro Costa <alejandroesfc@gmail.com>
2021-12-10 14:22:52 -0300

    unit tests for class Messages of the package messages

---
+++ TestMessages.java
@@ -0,0 +1,23 @@
+import messages.Messages;
+/**
+* Unit tests for class messages
+* @author Alejandro Costa
+* @version 0.1 2021-12-09
+*/
+public class TestMessages
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println();
+        System.out.println("Creating object of class Messages...");
+        Messages messagesToUser = new Messages();
+        System.out.println("Done.\n");
+
+        System.out.println("Exhibiting a welcome message...");
+        System.out.println("Result should be 'Welcome to Calculator!'. >>> Result: "); messagesToUser.welcomeMessage();
+        System.out.println();
+        System.out.println("Done.\n");
+
+    }
+}
